f22aa770b0e94f3468f16423ec4ac5f6e7a12fb6
Rahul <rahul97811@gmail.com>
2021-01-25 16:38:59 +0530

    Added background image

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 class Contact < ApplicationRecord
-  validates :first_name, :last_name, :email, :phone, presence: true
+  validates :first_name, :last_name, :email, :message, presence: true
   validates :email, uniqueness: true
 
   after_commit :send_notification, on: :create
 
   private
 
+  # send notification to admin
   def send_notification
     ContactMailer.send_contact_email(id).deliver_now
   end
